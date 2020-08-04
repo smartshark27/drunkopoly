de006352ca93b87bf686d8a4daf523acde7a030c
smartshark27 <thomassmart97@gmail.com>
2020-08-05 00:10:38 +1000

    Fix font not showing on Android

--- src/style.js
+++ src/style.js
@@ -12,5 +12,6 @@ const COLORS = {
 
 const FONTS = {
   LUCIDA_CONSOLE: "font-family: 'Lucida Console', Monaco, monospace;",
-  COMIC_SANS: 'font-family: "Comic Sans MS", cursive, sans-serif',
+  COMIC_SANS: 'font-family: "Comic Sans MS", cursive, sans-serif;',
+  PATRICK: "font-family: 'Patrick Hand', cursive, sans-serif;"
 };
