839b150a3317ee44cefb14e1cacb77f7e74614d1
smartshark27 <thomassmart97@gmail.com>
2020-08-23 16:28:29 +1000

    Replace dice num with pattern

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,22 +1,25 @@
 class Dice extends Component {
   constructor() {
     super();
+    this.total = 0;
     this._draw();
   }
 
   roll(next) {
     console.log("Rolling dice");
-    this._rollDie(this.leftDie, DIE_LEFT_ROLL_TIME);
-    this._rollDie(this.rightDie, DIE_RIGHT_ROLL_TIME, next);
+    this._rollDie(this.leftDots, DIE_LEFT_ROLL_TIME);
+    this._rollDie(this.rightDots, DIE_RIGHT_ROLL_TIME, next);
   }
 
   getRolledNum() {
-    return Number(this.leftDie.getTextContent()) + Number(this.rightDie.getTextContent());
+    return this.total;
   }
 
   _draw() {
-    this.leftDie = this._drawDie(-DICE_SEPARATION / 2 - DIE_SIZE);
-    this.rightDie = this._drawDie(DICE_SEPARATION / 2);
+    this._drawDie(-DICE_SEPARATION / 2 - DIE_SIZE);
+    this._drawDie(DICE_SEPARATION / 2);
+    this.leftDots = this._drawDots(-DICE_SEPARATION / 2 - DIE_SIZE);
+    this.rightDots = this._drawDots(DICE_SEPARATION / 2);
   }
 
   _drawDie(x) {
@@ -41,29 +44,53 @@ class Dice extends Component {
         .setAttribute("height", DIE_SIZE)
         .setAttribute("fill", COLORS.WHITE)
     );
-    const dieNum = SVG.new("text")
-      .setAttribute("dominant-baseline", "middle")
-      .setAttribute("text-anchor", "middle")
-      .setAttribute("x", x + DIE_SIZE / 2)
-      .setAttribute("style", FONTS.PATRICK_HAND)
-      .setAttribute("font-size", DIE_NUM_SIZE)
-      .setAttribute("fill", COLORS.BLACK);
-    this.addElement(dieNum);
-    return dieNum;
   }
 
-  _rollDie(die, time, next = () => {}) {
+  _drawDots(dieLeftX) {
+    const topY = -DIE_SIZE / 2 - DIE_Y_OFFSET + DIE_SIZE / 6;
+    const leftX = dieLeftX + DIE_SIZE / 6;
+    const dots = [];
+    for (var y = 0; y < 3; y++) {
+      for (var x = 0; x < 3; x++) {
+        const dot = SVG.new("circle")
+          .setAttribute("cx", leftX + (x * DIE_SIZE) / 3)
+          .setAttribute("cy", topY + (y * DIE_SIZE) / 3)
+          .setAttribute("r", DIE_DOT_RADIUS)
+          .setAttribute("fill", COLORS.BLACK)
+          .hide();
+        dots.push(dot);
+        this.addElement(dot);
+      }
+    }
+    return dots;
+  }
+
+  _rollDie(dots, time, next = () => {}) {
+    var num = 0;
     const interval = setInterval(() => {
-      const num = generateRandomNumberBetween(1, DIE_NUMBERS);
-      this._setDieNum(die, num);
+      num = generateRandomNumberBetween(1, DIE_NUMBERS);
+      this._setDots(dots, num);
     }, FRAME_DELAY);
     sleep(time).then(() => {
       clearInterval(interval);
+      this.total += num;
       next();
     });
   }
 
-  _setDieNum(die, num) {
-    die.setTextContent(num);
+  _setDots(dots, num) {
+    const patterns = {
+      0: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      1: [0, 0, 0, 0, 1, 0, 0, 0, 0],
+      2: [1, 0, 0, 0, 0, 0, 0, 0, 1],
+      3: [1, 0, 0, 0, 1, 0, 0, 0, 1],
+      4: [1, 0, 1, 0, 0, 0, 1, 0, 1],
+      5: [1, 0, 1, 0, 1, 0, 1, 0, 1],
+      6: [1, 1, 1, 0, 0, 0, 1, 1, 1],
+    };
+    const pattern = patterns[num];
+    for (var i = 0; i < pattern.length; i++) {
+      dots[i].setAttribute("opacity", pattern[i]);
+    }
   }
 }
